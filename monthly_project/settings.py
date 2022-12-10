3659143be111f3bdcf2dc20fdba4d72f036db3ef
Sai Vashist <saivashistdasyapu@gmail.com>
2022-12-10 13:24:49 +0530

    canging keys

--- monthly_project/settings.py
+++ monthly_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b%2@rl34y^ryl13zcvl9q!d9v0sx&gz$7jnhml^pyx))jdhfja'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
